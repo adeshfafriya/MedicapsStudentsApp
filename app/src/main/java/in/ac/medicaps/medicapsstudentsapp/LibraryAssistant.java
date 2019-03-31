0ed3eccac5dc895954f020efbbe2b164a0959407
Samarth Paliwal <ram.paliwal49@gmail.com>
2019-03-31 11:39:37 +0530

    new activity added LibraryAssistant (only xml)

---
+++ app/src/main/java/in/ac/medicaps/medicapsstudentsapp/LibraryAssistant.java
@@ -0,0 +1,13 @@
+package in.ac.medicaps.medicapsstudentsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibraryAssistant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_assistant);
+    }
+}
