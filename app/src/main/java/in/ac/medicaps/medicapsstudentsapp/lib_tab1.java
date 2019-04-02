411b280f041f2a216b6e1cedfa7024f810a70d16
Samarth Paliwal <ram.paliwal49@gmail.com>
2019-04-02 21:33:31 +0530

    Timepicker running :)

--- app/src/main/java/in/ac/medicaps/medicapsstudentsapp/lib_tab1.java
+++ app/src/main/java/in/ac/medicaps/medicapsstudentsapp/lib_tab1.java
@@ -2,6 +2,7 @@ package in.ac.medicaps.medicapsstudentsapp;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.app.Dialog;
 
 import android.os.Bundle;
@@ -19,13 +20,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.sql.Time;
 import java.util.Calendar;
+
 
 
 public class lib_tab1 extends AppCompatActivity {
 
-
+//below variables are for date picker
     Button btn;
     TextView date;
     DatePickerDialog datePicker;
@@ -34,12 +40,21 @@ public class lib_tab1 extends AppCompatActivity {
     int day_x;
     Calendar calendar;
 
+//    Below variables are for time picker
+    EditText choose;
+    TimePickerDialog PickerDialog;
+    int currentHour;
+    int currentMinute;
+    String amPm;
 
+
+//    below are some default codes
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_lib_make_entry);
 
+//below setting are for datepicker
 
         btn = findViewById(R.id.btnDate);
         date = findViewById(R.id.tvSelectedDate);
@@ -60,6 +75,35 @@ public class lib_tab1 extends AppCompatActivity {
                 datePicker.show();
             }
         });
+
+//        Below Settings are for Timepicker
+
+        choose = findViewById(R.id.AlarmTime);
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                PickerDialog = new TimePickerDialog(lib_tab1.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        choose.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                    }
+                }, currentHour, currentMinute, false);
+
+                PickerDialog.show();
+            }
+        });
+
+
+
     }
 }
 
