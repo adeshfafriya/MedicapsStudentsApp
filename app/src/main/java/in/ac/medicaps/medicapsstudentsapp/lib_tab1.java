be688558362fa0d381e23ec75553f4b4ae658a72
Samarth Paliwal <ram.paliwal49@gmail.com>
2019-04-02 21:07:48 +0530

    DatePicker error solved

--- app/src/main/java/in/ac/medicaps/medicapsstudentsapp/lib_tab1.java
+++ app/src/main/java/in/ac/medicaps/medicapsstudentsapp/lib_tab1.java
@@ -23,35 +23,26 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 
-public class lib_tab1 extends Fragment {
+public class lib_tab1 extends AppCompatActivity {
 
 
     Button btn;
     TextView date;
-    DatePicker datePicker;
+    DatePickerDialog datePicker;
     int year_x;
     int month_x;
     int day_x;
     Calendar calendar;
-    static final int DIALOG_ID = 0;
-
-
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_lib_make_entry, container, false);
+        setContentView(R.layout.fragment_lib_make_entry);
 
 
-        btn = getActivity().findViewById(R.id.btnDate);
-        date = getActivity().findViewById(R.id.tvSelectedDate);
+        btn = findViewById(R.id.btnDate);
+        date = findViewById(R.id.tvSelectedDate);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,31 +51,15 @@ public class lib_tab1 extends Fragment {
                 year_x = calendar.get(Calendar.YEAR);
                 month_x = calendar.get(Calendar.MONTH);
                 day_x = calendar.get(Calendar.DAY_OF_MONTH);
-                getActivity().showDialog(DIALOG_ID);
+                datePicker = new DatePickerDialog(lib_tab1.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(android.widget.DatePicker datePicker, int year, int month, int day) {
+                        date.setText("do");
+                    }
+                }, year_x, month_x, day_x);
+                datePicker.show();
             }
         });
+    }
+}
 
-
-        DatePickerDialog.OnDateSetListener dpickerListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                year_x = year;
-                month_x = month;
-                day_x = dayOfMonth;
-
-//                Toast.makeText(lib_tab1.this, year_x + "/" + month_x + "/" + day_x, Toast.LENGTH_LONG.show());
-
-            }
-        };
-
-//        private Dialog onCreateDialog(int id){
-//            if (id == DIALOG_ID)
-////                return new DatePickerDialog(this, dpickerListener, year_x, month_x, day_x);
-//            return null;
-//        }
-
-
-
-
-        return view;
-    }}
